86892dfcabf3243ae3583e9b866b13b97bf35b8a
Alex Nadzharov <anadjarov@gmail.com>
2017-03-05 14:38:00 +0300

    object maker resize

--- qtpd_gui/cm_objectmaker.cpp
+++ qtpd_gui/cm_objectmaker.cpp
@@ -4,6 +4,17 @@ cm_objectmaker::cm_objectmaker()
 {
     this->setFixedSize(65,20);
 
+    connect(this,&QLineEdit::textEdited, this,&cm_objectmaker::editorChanged);
 
+}
+
+
+void  cm_objectmaker::editorChanged()
+{
+    QFont myFont("Monaco", 11);
+    QFontMetrics fm(myFont);
+    int new_w = fm.width(QString(this->text())) + 10;
+    new_w = (new_w<65) ? 65 : new_w;
+    this->setFixedWidth(new_w);
 
 }
--- qtpd_gui/cm_objectmaker.h
+++ qtpd_gui/cm_objectmaker.h
@@ -4,11 +4,18 @@
 #include <QWidget>
 #include <QLineEdit>
 
-
+////
+/// \brief creates lineedit box for new object
+///
 class cm_objectmaker : public QLineEdit
 {
 public:
     cm_objectmaker();
+private slots:
+    ////
+    /// \brief resize to text
+    ///
+    void editorChanged();
 };
 
 #endif // CM_OBJECTMAKER_H
