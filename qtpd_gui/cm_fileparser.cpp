9ff14079f98f8e7cdc352ad2d37bcac0cd34fc93
Alex Nadzharov <anadjarov@gmail.com>
2017-03-17 11:33:49 +0300

    load filename to patch window

--- qtpd_gui/cm_fileparser.cpp
+++ qtpd_gui/cm_fileparser.cpp
@@ -4,5 +4,6 @@ namespace qtpd
 {
 PatchWindow* FileParser::pdParserPrevWindow = 0;
 PatchWindow* FileParser::pdParserWindow = 0;
+PatchWindow* FileParser::pdParserFirstWindow = 0;
 std::string FileParser::pdParserFileName = "";
 }
