33fafae4511ad50feff05dcd7dba7737d96ae0c9
Alex Nadzharov <anadjarov@gmail.com>
2017-04-01 10:51:52 +0300

    UIDSP stub

---
+++ qtpd_gui/objects/UIDSP.cpp
@@ -0,0 +1,30 @@
+// (c) 2017 Alex Nadzharov
+// License: GPL3
+
+#include "UIToggle.h"
+
+namespace qtpd {
+
+UIToggle::UIToggle(UIObject* parent)
+    : UIObject(parent)
+{
+    //setPdObjectName("ui.toggle");
+
+    this->setFixedSize(20, 20);
+
+    this->setMouseTracking(true);
+
+    this->deselect();
+    this->clicked_ = false;
+
+    QPalette Pal(palette());
+    Pal.setColor(QPalette::Background, QColor(240, 240, 240));
+    this->setAutoFillBackground(true);
+    this->setPalette(Pal);
+
+    this->setMinimumBoxWidth(20);
+    this->setMinimumBoxHeight(20);
+
+    this->value_ = false;
+}
+}
